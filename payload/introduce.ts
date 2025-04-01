2985d63a0526f8f7e1fb4aa4527a1b5dc8850db5
wnjoon <wnjoon@gmail.com>
2025-04-01 11:04:46 +0900

    feat: change intruduction

--- payload/introduce.ts
+++ payload/introduce.ts
@@ -5,7 +5,7 @@ const introduce: IIntroduce.Payload = {
   disable: false,
 
   contents: [
-    'Experienced in distributed systems and security, and currently working in the blockchain field using Hyperledger Fabric, Ethereum, and Cosmos. Interested in how to provide good services to users through blockchain and web3 in the future.',
+    'Software engineer with a solid foundation in blockchain, expanding my focus to solving real-world problems across diverse domains. I actively leverage AI tools to adapt quickly and learn efficiently in a rapidly evolving tech landscape. Passionate about building reliable, meaningful systems through purposeful use of technology.',
   ],
   sign: 'Wonjoon Lee',
   latestUpdated: lastestUpdatedAt,
